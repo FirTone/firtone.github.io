d6824b2cf38e207fa1e33e27795489bf5605c6c8
FirTone <75525050+FirTone@users.noreply.github.com>
2020-12-26 22:50:04 +0800

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,4 +1,10 @@
 sudo -i
+apt-get install screen
+screen -S code
 wget https://github.com/cdr/code-server/releases/download/v3.8.0/code-server-3.8.0-linux-amd64.tar.gz
 tar xf code-server-3.8.0-linux-amd64.tar.gz
-cd vscode
+cd /root/code-server-3.8.0-linux-amd64
+export PASSWORD="1314"
+
+./code-server --port 1314 --host 0.0.0.0 --auth password
+#运行code-server
